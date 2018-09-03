890e1fab6d9c3b8d565848f9e3bbfe6af761b360
xiao那片星空那片海 <1617753505@qq.com>
2018-09-04 00:15:44 +0800

    add easy tcp server.cpp

--- tcp_server.h
+++ tcp_server.h
@@ -10,11 +10,32 @@
 	#include <sys/types.h>
 	#include <sys/socket.h>
 	#include <netinet/in.h>
+	#include <unistd.h>
 #endif
+#include <vector>
+#define CELL_SERVER_NUM  4
+#define INVAILD_SOCKET  ~0
+#define ERROR_SOCKET -1
+#define OK_SOCKET 0
+#define PORT 6677
+#define LISTEN_NUM 1024
+class Client {
 
+};
+class CellServer {
+
+};
 class TcpServer {
 public:
-	void virtual run() { };
+	TcpServer();
+	void virtual onRun();
+
+
+private:
+	short initSocket();
+private:
+	int m_socket;
+	std::vector<CellServer *> m_cells;
 };
 
 #endif
